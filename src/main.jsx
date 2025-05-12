ab2edb6c22f012c8f832bf9e00ba17ce0bde0915
Suhani Kumari <suhani123@gmail.com>
2025-05-13 00:39:26 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 // main.jsx
-import React from 'react';
+import ContextProvider from './context/context.jsx';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import ContextProvider from './context/context.jsx'; // Ensure the correct path
+ // Ensure the correct path
 
 // Use ReactDOM.createRoot instead of createRoot directly
 ReactDOM.createRoot(document.getElementById('root')).render(
