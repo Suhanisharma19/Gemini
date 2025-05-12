7ec5ee18440b71caf69e600c98a819df5608596f
Suhani Kumari <suhani123@gmail.com>
2025-05-13 00:22:55 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [React()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Allows using "@/context/context.jsx"
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
