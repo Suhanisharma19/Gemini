f8e7b0cbee24b910f12eac1d4af8f8ab37f37126
Suhani Kumari <suhani123@gmail.com>
2025-05-13 02:02:08 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gemini/', // Replace with your actual GitHub repo name
+  base: '/Gemini/', // Replace with your GitHub repo name
 });
